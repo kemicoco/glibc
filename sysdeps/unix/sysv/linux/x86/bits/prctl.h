547c8a7909510e80a1f1d0f64212e0d35ab7450a
H.J. Lu <hjl.tools@gmail.com>
2017-10-25 12:35:11 -0700

    x86: Extend arch_prctl syscall for CET control
     /* CET features:
        IBT:   GNU_PROPERTY_X86_FEATURE_1_IBT
        SHSTK: GNU_PROPERTY_X86_FEATURE_1_SHSTK
      */
    
     /* Return CET features in unsigned long *addr:
          features: addr[0].
          always_on: addr[1].
          shadow stack size for a new image: addr[2].
      */
     # define ARCH_CET_STATUS		0x3001
     /* Disable CET features in unsigned int features.  */
     # define ARCH_CET_DISABLE		0x3002
     /* Lock all CET features.  */
     # define ARCH_CET_LOCK			0x3003
     /* Control how CET features should be enabled when launching a new image.
        unsigned long *addr:
          features: addr[0].
          control: addr[1].
          shadow stack size for a new image: addr[2].
      */
     # define ARCH_CET_EXEC			0x3004
     /* Allocate a new shadow stack with __syscall_ulong_t *addr:
          IN: requested shadow stack size: *addr.
          OUT: allocated shadow stack address: *addr.
      */
     # define ARCH_CET_ALLOC_SHSTK		0x3005
     /* Update shadow stack with __syscall_ulong_t *addr:
          ssp: addr[0].
          return_addr1: addr[1].
          return_addr2: addr[2].
      */
     # define ARCH_CET_PUSH_SHSTK		0x3006
     /* Return legacy region bitmap info in unsigned long *addr:
         address: addr[0].
         size: addr[1].
      */
     # define ARCH_CET_LEGACY_BITMAP	0x3007
    
     /* Valid control values of ARCH_CET_EXEC:
        0: Enable CET features based on ELF property note.
        1: Always disable CET features.
        2: Always enable CET features.
      */
     # define ARCH_CET_EXEC_ELF_PROPERTY	0
     # define ARCH_CET_EXEC_ALWAYS_OFF	1
     # define ARCH_CET_EXEC_ALWAYS_ON	2
     # define ARCH_CET_EXEC_MAX		ARCH_CET_EXEC_ALWAYS_ON
    
    In exec* system calls, kernel checks CET bits in NT_GNU_PROPERTY_TYPE_0
    note of PT_NOTE segment of
    
    1. Interpreter if there is one.
    2. Program if there is no interpreter.
    
    Then
    
    1. Enable IBT/SHSTK if IBT/SHSTK bits are set or IBT/SHSTK bits of
    CET_EXEC_ENABLED are set.  If there is no interpreter, lock CET.
    2. Disable IBT/SHSTK if IBT/SHSTK bits aren't set.
    
    Glibc:
    
    1. If interpreter is used:
    
    Get CET status at the beginning ->
    If CET is enabled then
      disable CET if needed ->
      Lock CET ->
    Endif
    Continue.
    
    2. If interpreter isn't used,
    
    Get CET status at the beginning ->
    Continue.
    
    	* sysdeps/unix/sysv/linux/Makefile (sysdep_headers): Add
    	bits/prctl.h.
    	* sysdeps/unix/sysv/linux/bits/prctl.h: New file.
    	* sysdeps/unix/sysv/linux/x86/cpu-features.c: Likewise.
    	* sysdeps/unix/sysv/linux/x86/bits/prctl.h: Likewise.
    	* sysdeps/unix/sysv/linux/x86/exit-thread.h: Likewise.
    	* sysdeps/unix/sysv/linux/sys/prctl.h: Include <bits/prctl.h>.
    	* sysdeps/unix/sysv/linux/x86/dl-cet.c: Include <pthread.h>,
    	<sys/syscall.h> and <sys/prctl.h>.
    	(BITS_PER_WORD): New.
    	(BITMAP_FIRST_WORD_MASK): Likewise.
    	(BITMAP_LAST_WORD_MASK): Likewise.
    	(dl_cet_mark_legacy_region): Mark PT_LOAD segments with PF_X in
    	legacy code page bitmap.
    	(dl_cet_check): Put legacy shared objects in legacy bitmap if
    	needed.  Call arch_prctl to disable IBT and/or SHSTK as well as
    	lock CET if IBT or SHSTK is enabled.
    	* sysdeps/unix/sysv/linux/x86/dl-procruntime.c
    	(dl_x86_legacy_bitmap): New.
    	* sysdeps/x86/libc-start.c: Include <startup.h>.

---
+++ sysdeps/unix/sysv/linux/x86/bits/prctl.h
@@ -0,0 +1,71 @@
+/* Copyright (C) 2018 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+
+#ifndef _BITS_PRCTL_H
+#define _BITS_PRCTL_H	1
+
+#ifndef ARCH_CET_STATUS
+/* CET features:
+   IBT:   GNU_PROPERTY_X86_FEATURE_1_IBT
+   SHSTK: GNU_PROPERTY_X86_FEATURE_1_SHSTK
+ */
+/* Return CET features in unsigned long *addr:
+     features: addr[0].
+     always_on: addr[1].
+     shadow stack size for a new image: addr[2].
+ */
+# define ARCH_CET_STATUS	0x3001
+/* Disable CET features in unsigned int features.  */
+# define ARCH_CET_DISABLE	0x3002
+/* Lock all CET features.  */
+# define ARCH_CET_LOCK		0x3003
+/* Control how CET features should be enabled when launching a new image.
+   unsigned long *addr:
+     features: addr[0].
+     control: addr[1].
+     shadow stack size for a new image: addr[2].
+ */
+# define ARCH_CET_EXEC		0x3004
+/* Allocate a new shadow stack with __syscall_ulong_t *addr:
+     IN: requested shadow stack size: *addr.
+     OUT: allocated shadow stack address: *addr.
+ */
+# define ARCH_CET_ALLOC_SHSTK	0x3005
+/* Update shadow stack with __syscall_ulong_t *addr:
+     ssp: addr[0].
+     return_addr1: addr[1].
+     return_addr2: addr[2].
+ */
+# define ARCH_CET_PUSH_SHSTK	0x3006
+/* Return legacy region bitmap info in unsigned long *addr:
+     address: addr[0].
+     size: addr[1].
+ */
+# define ARCH_CET_LEGACY_BITMAP	0x3007
+
+/* Valid control values of ARCH_CET_EXEC:
+   0: Enable CET features based on ELF property note.
+   1: Always disable CET features.
+   2: Always enable CET features.
+ */
+# define ARCH_CET_EXEC_ELF_PROPERTY	0
+# define ARCH_CET_EXEC_ALWAYS_OFF	1
+# define ARCH_CET_EXEC_ALWAYS_ON	2
+# define ARCH_CET_EXEC_MAX		ARCH_CET_EXEC_ALWAYS_ON
+#endif /* ARCH_CET_STATUS */
+
+#endif  /* bits/prctl.h */
